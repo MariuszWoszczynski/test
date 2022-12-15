5ae67807527695181b4e74cb421cb8504b887104
MariuszWoszczynski <115429759+MariuszWoszczynski@users.noreply.github.com>
2022-12-15 19:30:03 +0100

    Update izar_wmbus.cpp

--- izar_wmbus.cpp
+++ izar_wmbus.cpp
@@ -43,7 +43,7 @@ void IzarWmbus::init(uint32_t waterMeter) {
 uint8_t IzarWmbus::ReceiveData2(byte* rxBuffer) {
     uint8_t size = ELECHOUSE_cc1101.SpiReadStatus(CC1101_RXBYTES) & 0x7F;
     if (size) {
-    //    ELECHOUSE_cc1101.SpiReadBurstReg(CC1101_RXFIFO, rxBuffer, size);
+        ELECHOUSE_cc1101.SpiReadBurstReg(CC1101_RXFIFO, rxBuffer, size);
     }
     //ELECHOUSE_cc1101.SpiStrobe(CC1101_SFRX);
     //ELECHOUSE_cc1101.SpiStrobe(CC1101_SRX);
